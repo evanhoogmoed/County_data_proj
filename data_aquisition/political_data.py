0bc3629723029b9f27d3825066b7aff2f77c576b
Emma Van Hoogmoed <56660574+evanhoogmoed@users.noreply.github.com>
2023-03-13 23:15:04 -0700

    Add FIPS code and restructure df

--- data_aquisition/political_data.py
+++ data_aquisition/political_data.py
@@ -1,17 +1,33 @@
 import pandas as pd
 
+#adds extra column to dataframe with unique identifier for each county
+def add_fips_code(df):
+    fips_df = pd.read_csv("../data/fips.csv")
+    fips_df = fips_df.applymap(lambda s:s.lower() if type(s) == str else s)
+    df["FIPS_Code"] = ""
+    for index, row in df.iterrows():
+        county = row["County"]
+        state = row["State_Abrev"]
+        fips_code = fips_df.loc[(fips_df["Area_name"] == county) & (fips_df["State"] == state)]['FIPS_Code']
+        fips_code = fips_code.to_numpy()
+        if len(fips_code) == 0:
+            print("no fips code found for county: ", county, " state: ", state)
+            continue
+        df.loc[index, "FIPS_Code"] = fips_code[0]
+    return df
+
 def main():
     #read in countypres csv
-    df = pd.read_csv("countypres_2000-2020.csv")
+    df = pd.read_csv("../data/countypres_2000-2020.csv")
     df = df[df["year"] == 2020]
-    #get only columns year, state, state_po, county, candidate, party, candidatevotes, totalvotes
     df = df[["year", "state", "state_po", "county_name", "candidate", "party", "candidatevotes", "totalvotes"]]
     #print(df.head(10))
-
-    #loop through every row in df
-    #for index, row in df.iterrows():
+    #remove states we are not considering
+    df = df[df["state_po"] != "AK"]
+    df = df[df["state_po"] != "DC"]
+    
     #create new dataframe
-    simplified_df = pd.DataFrame(columns = ["year", "state", "state_po", "county", "democrat","republican","other", "winner", "totalvotes"])
+    simplified_df = pd.DataFrame(columns = ["Year","State", "State_Abrev", "County", "Democrat","Republican","Other", "Winner", "Total_Votes"])
 
 
     #get all unique county and state pairs from df
@@ -39,20 +55,38 @@ def main():
         year = county_df["year"].iloc[0]
         #get state
         state = county_df["state"].iloc[0]
+
         #get state_po
         state_po = county_df["state_po"].iloc[0]
         #get county
         county = county_df["county_name"].iloc[0]
-        #add row to simplified_df
-        #simplified_df = simplified_df.append({"year": year, "state": state, "state_po": state_po, "county": county, "democrat": democrat, "republican": republican, "other": other, "winner": winner, "totalvotes": totalvotes}, ignore_index=True)
-        #add row to simplified_df using concat
-        simplified_df = pd.concat([simplified_df, pd.DataFrame({"year": year, "state": state, "state_po": state_po, "county": county, "democrat": democrat, "republican": republican, "other": other, "winner": winner, "totalvotes": totalvotes}, index=[0])], ignore_index=True)
+
+        #if statements to handle the odd ball cases and to add parish and county where needed
+        if state == "LOUISIANA":
+            county = county + " parish"
+
+        elif county[-5:] != " CITY" and county[-7:] != " COUNTY" and state != "LOUISIANA":
+            county = county + " county"
+
+        if state_po == "VA" and county == "JAMES CITY" or state_po == "VA" and county == "CHARLES CITY":
+            county = county + " county"
+         
+
+        #if county begins with SAINT replace it with st.
+        if county[0:5] == "SAINT":
+            county = "ST." + county[5:]
+        
+        if county[0:3] == "ST ":
+            county = "ST. " + county[3:]
+
+        simplified_df = pd.concat([simplified_df, pd.DataFrame({"Year": year, "State": state, "State_Abrev": state_po, "County": county, "Democrat": democrat, "Republican": republican, "Other": other, "Winner": winner, "Total_Votes": totalvotes}, index=[0])], ignore_index=True)
 
 
     #for all rows in simplified_df make all strings lowercase
     simplified_df = simplified_df.applymap(lambda s:s.lower() if type(s) == str else s)
+    simplified_df = add_fips_code(simplified_df)
     #write simplified_df to csv
-    simplified_df.to_csv("counting_voting_info.csv", index=False)
+    simplified_df.to_csv("../data/voting_info.csv", index=False)
 
 
 if __name__ == "__main__":
