7b2f3847548e1dd86ae5aee7617a211ffc6e9da6
Emma Van Hoogmoed <56660574+evanhoogmoed@users.noreply.github.com>
2023-03-15 16:55:25 -0700

    redo with new db structure

--- visualizations/graphs.py
+++ visualizations/graphs.py
@@ -17,14 +17,15 @@ def highest_income_counties(merged_df):
     #get the top 10 counties with the highest income and the party that won
     top_10 = merged_df.nlargest(10, "Income")
 
-  
-    #create column in top_10 with county,state_po pair
-    top_10["county,state_po"] = top_10["county"].str.title() + ", " + top_10["state_po"].str.upper()
-    
+    top_10["County,State_Abrev"] = top_10["County"].str.title() + ", " + top_10["State_Abrev"].str.upper()
+    #drop the word county or parish from county,state_abrev
+    top_10["County,State_Abrev"] = top_10["County,State_Abrev"].str.replace(" County", "")
+    top_10["County,State_Abrev"] = top_10["County,State_Abrev"].str.replace(" Parish", "")
 
+    
     #make a bar graph for these top 10 counties
-    colors = ["#FF6B6B" if x == "donald j trump" else "#2FADCC" if x == "joseph r biden jr" else "green" for x in top_10["winner"]]
-    top_10.plot.bar(x="county,state_po", y="Income", rot=0, color=colors)
+    colors = ["#FF6B6B" if x == "donald j trump" else "#2FADCC" if x == "joseph r biden jr" else "green" for x in top_10["Winner"]]
+    top_10.plot.bar(x="County,State_Abrev", y="Income", rot=0, color=colors)
 
     plt.title("Highest Income Counties")
     plt.xlabel("County")
@@ -36,21 +37,26 @@ def highest_income_counties(merged_df):
     plt.legend(handles=[red_patch, blue_patch])
 
     #adjust size of graph and text
-    plt.gcf().set_size_inches(12, 5)
+    plt.gcf().set_size_inches(14, 5)
     plt.xticks(fontsize=8)
 
-    plt.show()
+    #plt.show()
+    plt.savefig("high_income_political.svg")
+    plt.savefig("high_income_political.jpg")
 
 def lowest_income_counties(merged_df):
     #get the top 10 counties with the lowest income and the party that won
     bottom_10 = merged_df.nsmallest(10, "Income")
 
-    bottom_10["county,state_po"] = bottom_10["county"].str.title() + ", " + bottom_10["state_po"].str.upper()
+    bottom_10["County,State_Abrev"] = bottom_10["County"].str.title() + ", " + bottom_10["State_Abrev"].str.upper()
     
+    #drop the word county or parish from county,state_abrev
+    bottom_10["County,State_Abrev"] = bottom_10["County,State_Abrev"].str.replace(" County", "")
+    bottom_10["County,State_Abrev"] = bottom_10["County,State_Abrev"].str.replace(" Parish", "")
 
     #make a bar graph for these top 10 counties
-    colors = ["#FF6B6B" if x == "donald j trump" else "#2FADCC" if x == "joseph r biden jr" else "green" for x in bottom_10["winner"]]
-    bottom_10.plot.bar(x="county,state_po", y="Income", rot=0, color=colors)
+    colors = ["#FF6B6B" if x == "donald j trump" else "#2FADCC" if x == "joseph r biden jr" else "green" for x in bottom_10["Winner"]]
+    bottom_10.plot.bar(x="County,State_Abrev", y="Income", rot=0, color=colors)
 
     plt.title("Lowest Income Counties")
     plt.xlabel("County")
@@ -65,32 +71,33 @@ def lowest_income_counties(merged_df):
     plt.gcf().set_size_inches(12, 5)
     plt.xticks(fontsize=8)
 
-    plt.show()
+    #plt.show()
+    #save plt as html
+    plt.savefig("low_income_political.svg")
+    plt.savefig("low_income_political.jpg")
 
 
 def main():
     conn = db_connect()
     
     #get columns county, state_po, and winner from politics tabl
-    politics_df = pd.read_sql(sql=text("SELECT county, state_po, winner FROM politics"),con=conn.connect())
+    politics_df = pd.read_sql(sql=text("SELECT * FROM politics"),con=conn.connect())
 
     #get columns county, state_abbreviations, and income from economics table
-    economics_df = pd.read_sql(sql=text("SELECT County, State_Abbreviation, Income FROM economics"),con=conn.connect())
-    
-    #drop the word county or parish from the county column in econmics_df
-    economics_df["County"] = economics_df["County"].str.replace(" county", "")
-    economics_df["County"] = economics_df["County"].str.replace(" parish", "")
+    economics_df = pd.read_sql(sql=text("SELECT * FROM economics"),con=conn.connect())
 
     #merge the two dataframes
-    merged_df = pd.merge(politics_df, economics_df, how="inner", left_on=["county", "state_po"], right_on=["County", "State_Abbreviation"])
-    merged_df = merged_df.drop(columns=["County", "State_Abbreviation"])
+    merged_df = pd.merge(politics_df, economics_df, how="inner", left_on=["County","State_Abrev"], right_on=["County", "State_Abrev"])
 
-    #get the average income for each party
-    income_by_party = merged_df.groupby("winner").mean()
-    print(income_by_party)
 
     highest_income_counties(merged_df)
     lowest_income_counties(merged_df)
+
+
+
+
+
+    
 
 
 if __name__ == "__main__":
